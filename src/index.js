39fde8c01c8e4d11ce3132b5f514f870c8bf116c
ducctrong <trongld21@gmail.com>
2022-05-30 14:58:57 +0700

    Add ground, background, model

--- src/index.js
+++ src/index.js
@@ -1,106 +1,138 @@
 import keyInput from "./keyinput.js";
 import * as THREE from "../node_modules/three/build/three.module.js"
 import { GLTFLoader } from "../node_modules/three/examples/jsm/loaders/GLTFLoader.js";
+import { OrbitControls } from "../node_modules/three/examples/jsm/controls/OrbitControls.js";
 
 // Setup
-const ratio = window.innerWidth / window.innerHeight;
+// Scene
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xa8def0);
+
+// Camera
+const ratio = window.innerWidth / window.innerHeight;
 const camera = new THREE.PerspectiveCamera(75, ratio, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
+camera.position.set(5, 5, 0);
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 renderer.shadowMap.enable = true;
 renderer.gammaOuput = true;
+renderer.gammaFactor = 2.2;
 document.body.appendChild(renderer.domElement);
 
+// Controls
+const orbitControls = new OrbitControls(camera, renderer.domElement);
+orbitControls.enableDamping = true;
+orbitControls.minDistance = 5;
+orbitControls.maxDistance = 15;
+orbitControls.enablePan = false;
+orbitControls.maxPolarAngle = Math.PI / 2 - 0.05;
+orbitControls.update();
+
+// Model with animation
+new GLTFLoader().load('../assets/Soldier.glb', function(gltf) {
+    var model = gltf.scene;
+    model.traverse(function(object) {
+        if (object.isMesh) {
+            object.castShadow = true;
+        }
+    });
+    model.position.set(0, 0, 0)
+    scene.add(model);
+})
+
+// Control Keys
+const keysPressed = {}
+document.addEventListener('keydown', (event) => {
+    if (event.shiftKey) {
+
+    } else {
+        keysPressed[event.key.toLowerCase()] = true;
+    }
+}, false);
+document.addEventListener('keyup', function(event) {
+    keysPressed[event.key.toLowerCase()] = false;
+}, false);
+
 // Create Light
-const light = new THREE.AmbientLight(0x404040);
-const dlight = new THREE.DirectionalLight(0xffffff, 1);
+scene.add(new THREE.AmbientLight(0xffffff, 1))
 
-light.add(dlight);
-scene.add(light);
-
-// Create Enemy
-var root;
-const loader = new GLTFLoader();
-loader.load('../assets/wraith.glb', function(glb) {
-    console.log(glb);
-    root = glb.scene;
-    root.position.set(10, 3, 0);
-    root.scale.set(0.1, 0.1, 0.1);
-    scene.add(root);
-}, function(xhr) {
-    console.log((xhr.loaded / xhr.total * 100) + "% loaded");
-}, function(error) {
-    console.log("An error");
-})
-loader.load('../assets/house.glb', function(glb) {
-    console.log(glb);
-    root = glb.scene;
-    root.position.set(-10, 1, 10);
-    root.scale.set(3, 3, 3);
-    scene.add(root);
-}, function(xhr) {
-    console.log((xhr.loaded / xhr.total * 100) + "% loaded");
-}, function(error) {
-    console.log("An error");
-})
+const dirLight = new THREE.DirectionalLight(0xffffff, 1)
+dirLight.position.set(-60, 100, -10);
+dirLight.castShadow = true;
+dirLight.shadow.camera.top = 50;
+dirLight.shadow.camera.bottom = -50;
+dirLight.shadow.camera.left = -50;
+dirLight.shadow.camera.right = 50;
+dirLight.shadow.camera.near = 0.1;
+dirLight.shadow.camera.far = 200;
+dirLight.shadow.mapSize.width = 4096;
+dirLight.shadow.mapSize.height = 4096;
+scene.add(dirLight);
 
 // Create ground
+// TEXTURES
+const textureLoader = new THREE.TextureLoader();
+const placeholder = textureLoader.load("../assets/placeholder.png");
+const sandBaseColor = textureLoader.load("../assets/Sand 002_COLOR.jpg");
+const sandNormalMap = textureLoader.load("../assets/Sand 002_NRM.jpg");
+const sandHeightMap = textureLoader.load("../assets/Sand 002_DISP.jpg");
+const sandAmbientOcclusion = textureLoader.load("../assets/Sand 002_OCC.jpg");
 
-const geometry = new THREE.BoxGeometry(100, 1, 100);
-const material = new THREE.MeshPhongMaterial({ color: 0x614124 });
-const ground = new THREE.Mesh(geometry, material);
+const WIDTH = 80
+const LENGTH = 80
 
-scene.add(ground);
-camera.position.set(5, 15, 15);
+const geometry = new THREE.PlaneGeometry(WIDTH, LENGTH, 512, 512);
+const material = new THREE.MeshStandardMaterial({
+    map: sandBaseColor,
+    normalMap: sandNormalMap,
+    displacementMap: sandHeightMap,
+    displacementScale: 0.1,
+    aoMap: sandAmbientOcclusion
+})
+wrapAndRepeatTexture(material.map)
+wrapAndRepeatTexture(material.normalMap)
+wrapAndRepeatTexture(material.displacementMap)
+wrapAndRepeatTexture(material.aoMap)
 
-// Create panaroma
-// const geometryPanaroma = new THREE.SphereGeometry(100, 32, 32);
-// const textureLoader = new THREE.TextureLoader();
-// const texture = textureLoader.load("./assets/bg.jpg");
-// const materialPanaroma = new THREE.MeshBasicMaterial({
-//     map: texture,
-//     side: THREE.DoubleSide
-// })
-// const sphere = new THREE.Mesh(geometryPanaroma, materialPanaroma);
-// scene.add(sphere);
-
-// Event javascript
-
-// Create function animation
+const floor = new THREE.Mesh(geometry, material)
+floor.receiveShadow = true
+floor.rotation.x = -Math.PI / 2
+scene.add(floor)
+    // Create function animation
+var clock = new THREE.Clock();
 
 function animate() {
     requestAnimationFrame(animate);
     if (keyInput.isPressed(87)) {
-        // root.position.z -= 0.1;
-        camera.position.z -= 0.05;
+        // camera.position.z -= 0.05;
     }
     if (keyInput.isPressed(83)) {
-        // root.position.z += 0.1;
-        camera.position.z += 0.05;
+        // camera.position.z += 0.05;
     }
     if (keyInput.isPressed(65)) {
-        // root.position.x -= 0.1;
-        camera.position.x += 0.05;
+        // camera.position.x += 0.05;
     }
     if (keyInput.isPressed(68)) {
-        // root.position.x += 0.1;
-        camera.position.x -= 0.05;
+        // camera.position.x -= 0.05;
     }
     if (keyInput.isPressed(81)) {
-        // root.position.y -= 0.1;
-        camera.position.y += 0.05;
+        // camera.position.y += 0.05;
     }
     if (keyInput.isPressed(69)) {
-        // root.position.y += 0.1;
-        camera.position.y -= 0.05;
+        // camera.position.y -= 0.05;
     }
-    camera.lookAt(ground.position);
-    root.rotation.y += 0.01;
+    camera.lookAt(scene.position);
     renderer.render(scene, camera);
 }
 animate();
+
+function wrapAndRepeatTexture(map) {
+    map.wrapS = map.wrapT = THREE.RepeatWrapping;
+    map.repeat.x = map.repeat.y = 10;
+}
 
 // Loop for attribute of object
 // scene.traverse(function(child) {
