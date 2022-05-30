b37386c1be68dbd8e6ab4b6ca166c11f4a55d26c
ducctrong <trongld21@gmail.com>
2022-05-30 13:11:18 +0700

    add .gitignore, add entity

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ document.body.appendChild(renderer.domElement);
 
 // Create Light
 const light = new THREE.AmbientLight(0x404040);
-const dlight = new THREE.DirectionalLight(0xffffff, 0.5);
+const dlight = new THREE.DirectionalLight(0xffffff, 1);
 
 light.add(dlight);
 scene.add(light);
@@ -26,23 +26,45 @@ const loader = new GLTFLoader();
 loader.load('../assets/wraith.glb', function(glb) {
     console.log(glb);
     root = glb.scene;
-    root.position.set(0, 1, 0)
-    root.scale.set(0.1, 0.1, 0.1)
-    scene.add(root)
+    root.position.set(10, 3, 0);
+    root.scale.set(0.1, 0.1, 0.1);
+    scene.add(root);
 }, function(xhr) {
-    console.log((xhr.loaded / xhr.total * 100) + "% loaded")
+    console.log((xhr.loaded / xhr.total * 100) + "% loaded");
 }, function(error) {
-    console.log("An error")
+    console.log("An error");
+})
+loader.load('../assets/house.glb', function(glb) {
+    console.log(glb);
+    root = glb.scene;
+    root.position.set(-10, 1, 10);
+    root.scale.set(3, 3, 3);
+    scene.add(root);
+}, function(xhr) {
+    console.log((xhr.loaded / xhr.total * 100) + "% loaded");
+}, function(error) {
+    console.log("An error");
 })
 
 // Create ground
 
-const geometry = new THREE.BoxGeometry(50, 1, 50);
-const material = new THREE.MeshPhongMaterial({ color: 0xffffff });
+const geometry = new THREE.BoxGeometry(100, 1, 100);
+const material = new THREE.MeshPhongMaterial({ color: 0x614124 });
 const ground = new THREE.Mesh(geometry, material);
 
 scene.add(ground);
 camera.position.set(5, 15, 15);
+
+// Create panaroma
+// const geometryPanaroma = new THREE.SphereGeometry(100, 32, 32);
+// const textureLoader = new THREE.TextureLoader();
+// const texture = textureLoader.load("./assets/bg.jpg");
+// const materialPanaroma = new THREE.MeshBasicMaterial({
+//     map: texture,
+//     side: THREE.DoubleSide
+// })
+// const sphere = new THREE.Mesh(geometryPanaroma, materialPanaroma);
+// scene.add(sphere);
 
 // Event javascript
 
@@ -51,27 +73,27 @@ camera.position.set(5, 15, 15);
 function animate() {
     requestAnimationFrame(animate);
     if (keyInput.isPressed(87)) {
-        root.position.z -= 0.1;
+        // root.position.z -= 0.1;
         camera.position.z -= 0.05;
     }
     if (keyInput.isPressed(83)) {
-        root.position.z += 0.1;
+        // root.position.z += 0.1;
         camera.position.z += 0.05;
     }
     if (keyInput.isPressed(65)) {
-        root.position.x -= 0.1;
+        // root.position.x -= 0.1;
         camera.position.x += 0.05;
     }
     if (keyInput.isPressed(68)) {
-        root.position.x += 0.1;
+        // root.position.x += 0.1;
         camera.position.x -= 0.05;
     }
     if (keyInput.isPressed(81)) {
-        root.position.y -= 0.1;
+        // root.position.y -= 0.1;
         camera.position.y += 0.05;
     }
     if (keyInput.isPressed(69)) {
-        root.position.y += 0.1;
+        // root.position.y += 0.1;
         camera.position.y -= 0.05;
     }
     camera.lookAt(ground.position);
